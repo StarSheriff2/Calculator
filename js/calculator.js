c3f3c8f0eb33c1014cc66386954d036696432991
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-06-13 14:06:57 -0600

    created calculator design, moved all operations into one single object

--- js/calculator.js
+++ js/calculator.js
@@ -1,29 +1,33 @@
-function add (addend, addend2) {
-	return addend + addend2;
-}
-
-function subtract (minuend, subtrahend) {
-	return minuend - subtrahend;
-}
-
-function multiply (array) {
-    return array.reduce((product, multiplier) => product * multiplier);
-}
-
-function divide (dividend, divisor) {
-    return (divisor != 0)? dividend/divisor: "invalid operation";
-}
-
-function operate(operand1, operator, operand2) {
-    return  (operator == "add")? add(operand1, operand2): 
-            (operator == "subtract")? subtract(operand1, operand2):
-            (operator == "multiply")? multiply([operand1, operand2]): 
-            divide (operand1, operand2);
-}
+const mathOperations = {
+    operate: function(operand1, operator, operand2) {
+        return  (operator == "add")? this.add(operand1, operand2): 
+                (operator == "subtract")? this.subtract(operand1, operand2):
+                (operator == "multiply")? this.multiply([operand1, operand2]): 
+                divide (operand1, operand2);
+    },
+    add: function(addend, addend2) {
+        return addend + addend2;
+    },
+    subtract: function(minuend, subtrahend) {
+        return minuend - subtrahend;
+    },
+    multiply: function(array) {
+        return array.reduce((product, multiplier) => product * multiplier);
+    },
+    divide: function(dividend, divisor) {
+        return (divisor != 0)? dividend/divisor: "invalid operation";
+    },
+};
 
 
-/*Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key.
 
-    Do not worry about wiring up the JS just yet.
-    There should also be a display for the calculator, go ahead and fill it with some dummy numbers so you can get it looking right.
-    Add a “clear” button.*/
+
+
+
+
+
+
+
+
+
+
