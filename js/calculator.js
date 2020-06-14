48a9ed39eb331dd71b6d446724e7abc1462641ef
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-06-13 18:26:33 -0600

    js wired into html. All operations work, including equals sign, except divide operator. DIsplay shows operation results, except for division.

--- js/calculator.js
+++ js/calculator.js
@@ -1,12 +1,58 @@
+const displayDiv = document.querySelector("#displayDiv");
+const displayStyle = displayDiv.style;
+const numberbtn = document.querySelectorAll(".numberbtn");
+const operatorBtn = document.querySelectorAll(".operatorBtn");
+let storedOperation;
+const kEquals = document.querySelector("#kEquals");
+
+
+Array.from(numberbtn).forEach(bt => bt.addEventListener("click", e => {
+    displayFunctions.display(e.target.id);
+}));
+
+Array.from(operatorBtn).forEach(bt => bt.addEventListener("click", e => {
+    console.log(e.target.id);
+    displayFunctions.storedOperator = (e.target.id);
+    storedOperation = [displayFunctions.storedOperand, displayFunctions.storedOperator];
+    displayFunctions.storedOperand = "";
+}));
+
+kEquals.addEventListener("click", _ => {
+    console.log(storedOperation[0],storedOperation[1], 
+        (displayFunctions.storedOperand == "")? storedOperation[0]: displayFunctions.storedOperand);
+        displayDiv.textContent = mathOperations.operate(storedOperation[0], storedOperation[1], 
+        (displayFunctions.storedOperand == "")? storedOperation[0]: displayFunctions.storedOperand);  
+});
+
+const displayFunctions = {
+    storedOperand: "",
+    storedOperator: "",
+    display: function(key) {
+        displayDiv.textContent = `${document.querySelector(`#${key}`).textContent}`;
+        this.storedOperand = displayDiv.textContent*1;
+        console.log(this.storedOperand);
+    },
+    /*storedOperation: function() {
+        const storedOperand = this.storedOperand;
+        const operand = storedOperand;
+        const operator = this.storedOperator;
+        console.log(storedOperand, operand);
+        console.log(operand, operator);
+        return operand, operator;
+    },*/
+};
+
+
 const mathOperations = {
     operate: function(operand1, operator, operand2) {
-        return  (operator == "add")? this.add(operand1, operand2): 
-                (operator == "subtract")? this.subtract(operand1, operand2):
-                (operator == "multiply")? this.multiply([operand1, operand2]): 
-                divide (operand1, operand2);
+        return  (operator == "kAdd")? this.add(operand1, operand2): 
+                (operator == "kSubtract")? this.subtract(operand1, operand2):
+                (operator == "kMultiply")? this.multiply([operand1, operand2]): 
+                divide(operand1, operand2);
     },
     add: function(addend, addend2) {
-        return addend + addend2;
+        console.log(addend + addend2);
+        return (addend + addend2);
     },
     subtract: function(minuend, subtrahend) {
         return minuend - subtrahend;
